35005e1837d60808e1270ec4a605564e52d977da
MrDevil159 <mehfoozmaaz@gmail.com>
2023-06-21 00:06:38 +0530

    #router used

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Routes basename="/react-training3">
+      <Routes>
         <Route path="/*" element={<App />}/>
       </Routes>
     </Router>
